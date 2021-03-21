287b59d322e4478d201474bad560fc0e28c4625f
Zhumakhan <zhumakhan.nazir@nu.edu.kz>
2021-03-21 08:13:01 +0600

    text fixes

--- bot.py
+++ bot.py
@@ -25,9 +25,7 @@ models.Base.metadata.create_all(bind=engine)
 #to drop table
 # models.Dataset1.__table__.drop(bind=engine)
 
-
-
-
+time_spent_per_week_map = {0:"<2 hours", 1:"<2 hours", 2:"2 to 5 hours", 3:"5 to 10 hours", 4:">10 hours"}
 #CREATING ENTRY
 entry1 = models.Dataset1(
     studytime = 1,
@@ -56,11 +54,11 @@ def get_avg(grade):
             func.avg(models.Dataset1.health).label('avg_health'),
             func.avg(models.Dataset1.absences).label('avg_absences')
             ).filter(models.Dataset1.G3==grade).first()
-    return avg
-
+    print(avg)
+    return f'studytime: {time_spent_per_week_map[ int(avg[0])%5 ]}\n activities: {str(round(avg[1]*100,2))} % yes\n freetime: {str(round(avg[3],2))} hours\n internet: {str(round(avg[2]*100,2))}% yes\n health: {str(round(avg[4],2))}\n absences: {str(round(avg[5],2))}'
 
 def get_model_prediction( studytime, activities, freetime, internet, health, absences):
-    payload="{\n  \"data\": [\n    {\n      \"studytime\": " + studytime + ",\n      \"activities\": " + activities + ",\n      \"internet\": " + internet + ",\n      \"freetime\": " + freetime + ",\n      \"health\": " + health + ",\n      \"absences\": " + absences + "\n    }\n  ]\n}"
+    payload="{\n  \"data\": [\n    {\n      \"studytime\": " + str(studytime) + ",\n      \"activities\": " + str(activities) + ",\n      \"internet\": " + str(internet) + ",\n      \"freetime\": " + str(freetime) + ",\n      \"health\": " + str(health) + ",\n      \"absences\": " + str(absences) + "\n    }\n  ]\n}"
     headers = {'Content-Type': 'application/json'}
     response = requests.post(CONFIG.ML_URL, headers=headers, data=payload)
     data = json.loads(response.json())
@@ -96,7 +94,7 @@ class MyBot(ActivityHandler):
             if member.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity(
                     MessageFactory.text(
-                        f"Welcome to the Exam Performance Boost bot, { member.name }."
+                        f"Welcome to the Exam Performance Boost bot, { member.name }. Type '/start' or '/restart' to begin"
                         f" Please, follow the instructions."
                     )
                 )
@@ -115,7 +113,7 @@ class MyBot(ActivityHandler):
             self.option = 1
             if self.on_score:
                 avg = get_avg(turn_context.activity.text)
-                send_text = MessageFactory.text(f"Parameteres are { avg }")
+                send_text = MessageFactory.text(f"Average perforances: { avg }")
                 return await turn_context.send_activity(send_text)
             await self._get_user_prefer_score(text, turn_context)
 
@@ -124,7 +122,7 @@ class MyBot(ActivityHandler):
             await self._get_user_performance(text, turn_context)
             if self.q_id == 7:
                 score_predict = get_model_prediction( self.studytime, self.activities, self.freetime, self.internet, self.health, self.absences )
-                send_text = MessageFactory.text(f"Probably you will get a '{ score_predict }'")
+                send_text = MessageFactory.text(f"Probably the score will be '{ score_predict }'")
                 return await turn_context.send_activity(send_text)
         
         if text == "option_get_rec" or self.option == 3:
@@ -138,7 +136,7 @@ class MyBot(ActivityHandler):
     
     async def _get_user_prefer_score(self, text : str, turn_context: TurnContext):
         self.on_score = True
-        send_text = MessageFactory.text("Choose score you want to look at?")
+        send_text = MessageFactory.text("Choose the score you want to look at?")
         send_text.suggested_actions = SuggestedActions(
             actions=[
                 CardAction(
@@ -174,7 +172,7 @@ class MyBot(ActivityHandler):
         if self.on_start:
             self.q_id += 1
         if self.q_id == 1:
-            send_text = MessageFactory.text("Choose your study time?")
+            send_text = MessageFactory.text("Time spent on study per week?")
             send_text.suggested_actions = SuggestedActions(
                 actions=[
                     CardAction(
@@ -202,7 +200,7 @@ class MyBot(ActivityHandler):
             return await turn_context.send_activity(send_text)
         if self.q_id == 2:
             self.studytime = turn_context.activity.text.lower()
-            send_text = MessageFactory.text("Please, evalute your free time after school (1 - very bad to 5 - very good).")
+            send_text = MessageFactory.text("Please, evaluate your free time after school (1 - less to 5 - more).")
             send_text.suggested_actions = SuggestedActions(
                 actions=[
                     CardAction(
@@ -271,7 +269,7 @@ class MyBot(ActivityHandler):
             return await turn_context.send_activity(send_text)
         if self.q_id == 5:
             self.internet = turn_context.activity.text.lower()
-            send_text = MessageFactory.text("Please, choose your current health status (1 - very bad to 5 - very good).")
+            send_text = MessageFactory.text("Please, choose your current health status (1 - unsatisfactory to 5 - good).")
             send_text.suggested_actions = SuggestedActions(
                 actions=[
                     CardAction(
@@ -363,17 +361,17 @@ class MyBot(ActivityHandler):
         reply.suggested_actions = SuggestedActions(
             actions=[
                 CardAction(
-                    title="Show performance for certain score",
+                    title="Get average performance per score",
                     type=ActionTypes.im_back,
                     value="option_show"
                 ),
                 CardAction(
-                    title="Predict my exam score",
+                    title="Predict exam score",
                     type=ActionTypes.im_back,
                     value="option_pred"
                 ),
                 CardAction(
-                    title="Improve your performance",
+                    title="Get tips for preparation",
                     type=ActionTypes.im_back,
                     value="option_get_rec"
                 ),
